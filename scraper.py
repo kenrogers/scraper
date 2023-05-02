d20f55b55b8dcfb232f05ac3ab620220ad38ef84
kenny <krogers@hey.com>
2023-05-02 10:50:03 -0600

    New website scraping

--- scraper.py
+++ scraper.py
@@ -8,11 +8,11 @@ import openai
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 # Function to fetch the webpage and extract the content of the target div
-def fetch_directory_search():
-    url = "https://www.causeiq.com/directory/amateur-sports-clubs-list/"
+def fetch_website_info():
+    url = "https://www.washingtonpremierfc.com/about-us/about-us/"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    directory_search = soup.find("div", class_="directory-search")
+    directory_search = soup.find("div", class_="inner-pagecontent")
     return directory_search.get_text(strip=True)
 
 # Function to query the OpenAI API
@@ -25,31 +25,19 @@ def query_openai_api(prompt):
     )
     return response.choices[0].text.strip()
 
-# Function to format the extracted information as a Markdown list
-def format_info_as_markdown(info):
-    # Escape dollar signs to avoid LaTeX formatting
-    info = info.replace('$', '\\$')
-
-    lines = info.split('\n')
-    markdown_lines = [f"- {line}" for line in lines if line.strip()]
-    return '\n'.join(markdown_lines)
-
 
 # Main app
-st.title("Amateur Sports Clubs Information Extractor")
+st.title("Washington Premier Football Club Analysis")
 
 # Fetch the directory search content
-directory_search_text = fetch_directory_search()
+website_text = fetch_website_info()
 
 # Prepare the OpenAI API prompt
-prompt = f"Extract the name, revenue, assets, and employees of each club from the following text:\n\n{directory_search_text}"
+prompt = f"The following text is a description of a sports club. Based on the information given, list out the key competitive achievements of the organization :\n\n{website_text}"
 
 # Call the OpenAI API
 extracted_info = query_openai_api(prompt)
 
-# Format the extracted information as a Markdown list
-formatted_info = format_info_as_markdown(extracted_info)
-
 # Display the extracted information
 st.write("Extracted information:")
-st.write(formatted_info)
+st.write(extracted_info)
